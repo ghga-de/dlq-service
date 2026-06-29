1d9bb95fd4a53366a707a9ec4431965f1383ee4f
Byron Himes <thebyronhimes@gmail.com>
2026-06-29 15:22:57 +0200

    Update src/dlqs/ports/outbound/dao.py
    Co-authored-by: Christoph Zwerschke <c.zwerschke@dkfz-heidelberg.de>

--- src/dlqs/ports/outbound/dao.py
+++ src/dlqs/ports/outbound/dao.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Dao and Aggregator port definitions"""
+"""DAO definition"""
 
 from hexkit.protocols.dao import Dao, ResourceNotFoundError  # noqa: F401
 
